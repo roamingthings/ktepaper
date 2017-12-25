083845ab666007fc9299d4b82bdadb5d3d8db8fc
Alexander Sparkowsky <info@roamingthings.de>
2017-12-25 10:32:34 +0100

    Update script to start anonymous docker container for build

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker run --rm -it -v /Users/alxs/Development/Workspaces/kotlin-native/ktepaper:/home/gradle -v /Users/alxs/.ssh:/home/gradle/.ssh -v /Users/alxs/.konan:/home/gradle/.konan -v /Users/alxs/.gradle:/home/gradle/.gradle -w /home/gradle --name kotlinc roamingthings/kotlin-native /bin/bash
+docker run --rm -it -v /Users/alxs/Development/Workspaces/kotlin-native/ktepaper:/home/gradle -v /Users/alxs/.ssh:/home/gradle/.ssh -v /Users/alxs/.konan:/home/gradle/.konan -v /Users/alxs/.gradle:/home/gradle/.gradle -w /home/gradle roamingthings/kotlin-native /bin/bash
